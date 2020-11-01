deca33978280f5fb9540c547549f5d3bbdc7b7d9
Ken Minami <kenminami.dev@gmail.com>
2020-11-01 21:36:54 +0900

    Mjpeg Streamer

--- main.go
+++ main.go
@@ -2,80 +2,66 @@ package main
 
 import (
 	"fmt"
-	"image"
-	"image/color"
+	"log"
+	"net/http"
 	"os"
-	"strconv"
 
+	"github.com/hybridgroup/mjpeg"
 	"gocv.io/x/gocv"
+)
+
+var (
+	deviceID int
+	err      error
+	webcam   *gocv.VideoCapture
+	stream   *mjpeg.Stream
 )
 
 func main() {
 	if len(os.Args) < 3 {
-		fmt.Println("How to run:\n\tfacedetect [camera ID] [classifier XML file]")
+		fmt.Println("How to run:\n\tmjpeg-streamer [camera ID] [host:port]")
 		return
 	}
 
 	// parse args
-	deviceID, _ := strconv.Atoi(os.Args[1])
-	xmlFile := os.Args[2]
+	deviceID := os.Args[1]
+	host := os.Args[2]
 
 	// open webcam
-	webcam, err := gocv.VideoCaptureDevice(int(deviceID))
+	webcam, err = gocv.OpenVideoCapture(deviceID)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("Error opening capture device: %v\n", deviceID)
 		return
 	}
 	defer webcam.Close()
 
-	// open display window
-	window := gocv.NewWindow("Face Detect")
-	defer window.Close()
+	// create the mjpeg stream
+	stream = mjpeg.NewStream()
 
-	// prepare image matrix
+	// start capturing
+	go mjpegCapture()
+
+	fmt.Println("Capturing. Point your browser to " + host)
+
+	// start http server
+	http.Handle("/", stream)
+	log.Fatal(http.ListenAndServe(host, nil))
+}
+
+func mjpegCapture() {
 	img := gocv.NewMat()
 	defer img.Close()
 
-	// color for the rect when faces detected
-	blue := color.RGBA{0, 0, 255, 0}
-
-	// load classifier to recognize faces
-	classifier := gocv.NewCascadeClassifier()
-	defer classifier.Close()
-
-	if !classifier.Load(xmlFile) {
-		fmt.Printf("Error reading cascade file: %v\n", xmlFile)
-		return
-	}
-
-	fmt.Printf("start reading camera device: %v\n", deviceID)
 	for {
 		if ok := webcam.Read(&img); !ok {
-			fmt.Printf("cannot read device %d\n", deviceID)
+			fmt.Printf("Device closed: %v\n", deviceID)
 			return
 		}
 		if img.Empty() {
 			continue
 		}
 
-		// detect faces
-		rects := classifier.DetectMultiScale(img)
-		fmt.Printf("found %d faces\n", len(rects))
-
-		// draw a rectangle around each face on the original image,
-		// along with text identifying as "Human"
-		for _, r := range rects {
-			gocv.Rectangle(&img, r, blue, 3)
-
-			size := gocv.GetTextSize("Human", gocv.FontHersheyPlain, 1.2, 2)
-			pt := image.Pt(r.Min.X+(r.Min.X/2)-(size.X/2), r.Min.Y-2)
-			gocv.PutText(&img, "Human", pt, gocv.FontHersheyPlain, 1.2, blue, 2)
-		}
-
-		// show the image in the window, and wait 1 millisecond
-		window.IMShow(img)
-		if window.WaitKey(1) >= 0 {
-			break
-		}
+		buf, _ := gocv.IMEncode(".jpg", img)
+		stream.UpdateJPEG(buf)
 	}
 }
