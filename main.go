a4336f293f7e000fe11f1928e51c2a8ca2676f0a
Ken Minami <kenminami.dev@gmail.com>
2020-11-04 19:51:42 +0900

    ドローンモデル作成

--- main.go
+++ main.go
@@ -1,43 +1,13 @@
 package main
 
 import (
+	"gotello/app/models"
 	"time"
-
-	"gobot.io/x/gobot"
-	"gobot.io/x/gobot/platforms/dji/tello"
 )
 
 func main() {
-	drone := tello.NewDriver("8889")
-
-	work := func() {
-		drone.TakeOff()
-
-		gobot.After(10*time.Second, func() {
-			drone.FrontFlip()
-		})
-
-		gobot.After(20*time.Second, func() {
-			drone.BackFlip()
-		})
-
-		gobot.After(30*time.Second, func() {
-			drone.RightFlip()
-		})
-
-		gobot.After(40*time.Second, func() {
-			drone.LeftFlip()
-		})
-
-		gobot.After(50*time.Second, func() {
-			drone.Land()
-		})
-	}
-
-	robot := gobot.NewRobot("tello",
-		[]gobot.Connection{},
-		[]gobot.Device{drone},
-		work)
-
-	robot.Start()
+	droneManager := models.NewDroneManager()
+	droneManager.TakeOff()
+	time.Sleep(5 * time.Second)
+	droneManager.Land()
 }
