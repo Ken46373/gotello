c4b93ce5c52cd4480dac0042066537f4907b7abf
Ken Minami <kenminami.dev@gmail.com>
2020-11-05 19:54:07 +0900

    レイアウトページ作成

--- app/controllers/webserver.go
+++ app/controllers/webserver.go
@@ -7,8 +7,12 @@ import (
 	"net/http"
 )
 
+func getTemplate(temp string) (*template.Template, error) {
+	return template.ParseFiles("app/views/layout.html", temp)
+}
+
 func viewIndexHandler(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("app/views/index.html")
+	t, _ := getTemplate("app/views/index.html")
 	err := t.Execute(w, nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
