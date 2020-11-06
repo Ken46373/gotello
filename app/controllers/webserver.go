8f0c9af437d5ad882b8672aa04e41b43fd514eb3
Ken Minami <kenminami.dev@gmail.com>
2020-11-06 19:39:51 +0900

    api実装

--- app/controllers/webserver.go
+++ app/controllers/webserver.go
@@ -1,10 +1,13 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"gotello/config"
 	"html/template"
+	"log"
 	"net/http"
+	"regexp"
 )
 
 func getTemplate(temp string) (*template.Template, error) {
@@ -27,9 +30,45 @@ func viewControllerHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type APIResult struct {
+	Result interface{} `json:"result"`
+	Code   int         `json:"code"`
+}
+
+func APIResponse(w http.ResponseWriter, result interface{}, code int) {
+	res := APIResult{Result: result, Code: code}
+	js, err := json.Marshal(res)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(js)
+}
+
+var apiValidPath = regexp.MustCompile("^/api/(command|shake|video)")
+
+func apiMakeHandler(fn func(w http.ResponseWriter, r *http.Request)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := apiValidPath.FindStringSubmatch(r.URL.Path)
+		if len(m) == 0 {
+			APIResponse(w, "Not Found", http.StatusNotFound)
+			return
+		}
+		fn(w, r)
+	}
+}
+
+func apiCommandHandler(w http.ResponseWriter, r *http.Request) {
+	command := r.FormValue("command")
+	log.Printf("action=apiCommandHandler command=%s", command)
+	APIResponse(w, "OK", http.StatusOK)
+}
+
 func StartWebServer() error {
 	http.HandleFunc("/", viewIndexHandler)
 	http.HandleFunc("/controller/", viewControllerHandler)
+	http.HandleFunc("/api/command/", apiMakeHandler(apiCommandHandler))
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	return http.ListenAndServe(fmt.Sprintf("%s:%d", config.Config.Address, config.Config.Port), nil)
 }
